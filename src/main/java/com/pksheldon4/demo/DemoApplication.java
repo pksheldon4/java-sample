c4a36284ab15c593c0d8176025fceb63b0085647
Preston Sheldon <pksiv@hotmail.com>
2021-03-19 21:50:02 -0400

    Initial commit

---
+++ src/main/java/com/pksheldon4/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.pksheldon4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "management.trace.http", name = "enabled", matchIfMissing = true)
+    public HttpTraceRepository inMemoryTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
+
+}
