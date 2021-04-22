dd330f97c5d6bf64454ebde063bb617476e1a80a
Preston Sheldon <pksiv@hotmail.com>
2021-04-22 16:01:51 -0400

    Updates to login with application in Login.gov sandbox

--- src/test/java/com/pksheldon4/demo/DemoApplicationTests.java
+++ src/test/java/com/pksheldon4/demo/DemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.pksheldon4.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("logingov")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
