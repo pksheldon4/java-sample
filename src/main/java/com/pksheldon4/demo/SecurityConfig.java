f0085016b626d958422d17e81d9ac84fefd275e5
Preston Sheldon <pksiv@hotmail.com>
2021-04-26 13:48:24 -0400

    Working Login.gov Oauth2 client

--- src/main/java/com/pksheldon4/demo/SecurityConfig.java
+++ src/main/java/com/pksheldon4/demo/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.pksheldon4.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -16,11 +15,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors()
             .and()
             .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/hello").hasAuthority("SCOPE_read")
-            .antMatchers(HttpMethod.GET, "/hellowrite").hasAuthority("SCOPE_write")
             .anyRequest().authenticated()
             .and()
             .oauth2Login();
     }
-
 }
